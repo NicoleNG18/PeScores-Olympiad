573dab1620f809fb02c33d6b090971c6c3323273
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-01-24 21:38:10 +0200

    Finish testing classes controller

--- src/test/java/pmgkn/pescores/pescores/web/ClassesControllerIT.java
+++ src/test/java/pmgkn/pescores/pescores/web/ClassesControllerIT.java
@@ -1,22 +1,46 @@
 package pmgkn.pescores.pescores.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pmgkn.pescores.pescores.util.TestDataUtils;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ClassesControllerIT {
 
-    //TODO: to make mock users and write tests with them
-
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void testClassesPageShowsUp(){
+    @Autowired
+    private TestDataUtils testDataUtils;
 
+    @BeforeEach
+    void setUp() {
+        testDataUtils.createTestUser("testEmail@abv.bg");
+    }
+
+    @AfterEach
+    void tearDown() {
+        testDataUtils.cleanUpDatabase();
+    }
+
+    @Test
+    @WithMockUser(username = "testEmail@abv.bg", roles = "USER")
+    void testClassesPageShowsUp() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/classes")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("classes"));
     }
 }
